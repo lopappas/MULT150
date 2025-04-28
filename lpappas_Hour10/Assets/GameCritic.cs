d27d65b3219e0dea72c96e53c920cfffefe0b1f5
Lorenzo Pappas <lopappas0@gmail.com>
2025-04-28 16:02:33 -0700

    Finished

---
+++ lpappas_Hour10/Assets/GameCritic.cs
@@ -0,0 +1,15 @@
+/*
+ * 
+ * 1. Improve player movement speed to help them engage with the game more meaningfully
+ * 2. Reduce ball speed and added very slight drag to make game less hectic and chaotic. The game now gets slightly easier and less chaotic over time.
+ * 3. Added cute sound effects for when the ball makes it in the goal, players will love this!
+ * 4. Quality of life changes - Removed all entity shadows from the game to reduce clutter. Removed footstep player sound. Changed player texture from red base color to granite texture.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
